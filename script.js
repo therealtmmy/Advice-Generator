ccea96603e2564a9870cbb717941239920035f7d
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-07-02 21:49:14 +0100

    light and dark mode added

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ const bookName = document.getElementById("bookname");
 const chapter = document.getElementById("Chapter");
 const verse = document.getElementById("Verse");
 
+// Light and Day
+const bodyPage = document.querySelector("body");
+const hero = document.getElementById("card");
+const nightTime = document.getElementById("night");
+const dayTime = document.getElementById("day");
+
+nightTime.addEventListener("click", () => {
+  bodyPage.style.setProperty("background", "hsl(232, 19%, 15%)");
+  hero.style.setProperty("background", "hsl(228, 28%, 20%)");
+  quote.style.setProperty("color", "hsl(0, 0%, 100%)");
+});
+
+dayTime.addEventListener("click", () => {
+  bodyPage.style.setProperty("background", "white");
+  hero.style.setProperty("background", "hsl(227, 47%, 96%)");
+  quote.style.setProperty("color", "hsl(230, 17%, 14%)");
+});
+
 // Fetch API
 button.addEventListener("click", () => {
   fetch("https://labs.bible.org/api/?passage=random&type=json")
@@ -14,7 +32,6 @@ button.addEventListener("click", () => {
     })
     .then((completedData) => {
       let array = completedData;
-      console.log(array);
       bookName.innerText = array[0].bookname;
       chapter.innerText = array[0].chapter;
       verse.innerText = array[0].verse;
@@ -22,6 +39,6 @@ button.addEventListener("click", () => {
     });
 });
 
-Date;
-let d = new Date();
-year.textContent = d.getFullYear();
+// Date;
+// let d = new Date();
+// year.innerText = d.getFullYear();
